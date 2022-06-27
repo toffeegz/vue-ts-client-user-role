179cecab84659e6c147cfa8d7c08883462bfabb4
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-27 15:25:41 +0800

    NavigationBar: route class and style bindings

--- src/components/layouts/NavigationBar.vue
+++ src/components/layouts/NavigationBar.vue
@@ -1,18 +1,18 @@
 <template>
-        <nav class="w-full flex justify-between py-4 px-8 bg-white border border-gray-200 shadow-sm shadow-red-200 ">
+        <nav class="w-full flex justify-between px-8 bg-white border border-gray-200 shadow-sm shadow-red-200 ">
             <div class="flex space-x-12">
-                <div class="flex items-center">
+                <div class="flex items-center  py-4">
                     <h3 class="text-xl font-medium text-gray-800">Laravel+Vue+Ts</h3>
                 </div>   
                 <!-- left header section -->
                 
-                <div class="items-center hidden space-x-4 lg:flex" v-if="authenticated">
-                    <div>
+                <div class="items-center hidden space-x-4 lg:flex font-semibold" v-if="authenticated">
+                    <div class="border-b-2 px-4 py-2 text-gray-400 tracking-wide border-transparent" :class="{ 'border-indigo-400 text-gray-700': currentRouteName === 'users' }">
                         <router-link :to="{ name: 'users' }">
-                            Users
+                            <span>Users</span>
                         </router-link>
                     </div>
-                    <div>
+                    <div class="border-b-2 px-4 py-2 text-gray-400 tracking-wide border-transparent" :class="{ 'border-indigo-400 text-gray-700': currentRouteName === 'roles' }">
                         <router-link :to="{ name: 'roles'}">
                             Roles
                         </router-link>
@@ -22,7 +22,7 @@
             </div>
             
             <!-- right header section -->
-            <div class="">
+            <div class=" py-4">
                 <template v-if="authenticated" class="relative text-left dropdown">
                     <span class="rounded-md shadow-sm">
                         <button @click="userDropdown = !userDropdown" class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800" type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
@@ -58,7 +58,10 @@
             ...mapGetters({
                 authenticated: 'auth/authenticated',
                 user: 'auth/user',
-            })
+            }),
+            currentRouteName() {
+                return this.$route.name;
+            }
         },
          data() {
             return {
