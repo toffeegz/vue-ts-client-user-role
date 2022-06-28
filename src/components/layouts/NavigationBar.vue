04baae921941848b6ba43f215e284ada393579c0
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-28 09:28:56 +0800

    Update NavigationBar.vue

--- src/components/layouts/NavigationBar.vue
+++ src/components/layouts/NavigationBar.vue
@@ -2,21 +2,13 @@
         <nav class="w-full flex justify-between px-8 bg-white border border-gray-200 shadow-sm shadow-red-200 ">
             <div class="flex space-x-12">
                 <div class="flex items-center  py-4">
-                    <h3 class="text-xl font-bold text-gray-800">Laravel+Vue</h3>
+                    <h3 class="text-xl font-bold text-gray-800">{{ name }}</h3>
                 </div>   
                 <!-- left header section -->
                 
                 <div class="items-center hidden space-x-4 lg:flex font-semibold" v-if="authenticated">
-                    <div class="border-b-2 px-4 py-2 text-gray-400 tracking-wide border-transparent" :class="{ 'border-indigo-400 text-gray-700': currentRouteName === 'users' }">
-                        <router-link :to="{ name: 'users' }">
-                            <span>Users</span>
-                        </router-link>
-                    </div>
-                    <div class="border-b-2 px-4 py-2 text-gray-400 tracking-wide border-transparent" :class="{ 'border-indigo-400 text-gray-700': currentRouteName === 'roles' }">
-                        <router-link :to="{ name: 'roles'}">
-                            Roles
-                        </router-link>
-                    </div>
+                    <LinkTab route_name="users"/>
+                    <LinkTab route_name="roles"/>
                 </div>
                 
             </div>
@@ -30,7 +22,7 @@
                             <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
                     </span>
-                    <div v-show="userDropdown" @focusout="handleFocusOut" class="dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+                    <div v-show="userDropdown" class="dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
                         <div class="mt-4 absolute right-0 w-56 origin-top-right bg-white border border-gray-300 divide-y divide-gray-100 rounded-md outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
                             <div class="px-4 py-3">         
                                 <p class="text-sm leading-5">Signed in as</p>
@@ -52,6 +44,7 @@
     import Vue from 'vue'
     import { defineComponent } from "vue"
     import { mapGetters, mapActions } from 'vuex'
+    import LinkTab from './LinkTab.vue'
 
     export default defineComponent({
         computed: {
@@ -59,12 +52,13 @@
                 authenticated: 'auth/authenticated',
                 user: 'auth/user',
             }),
-            currentRouteName() {
-                return this.$route.name;
-            }
         },
-         data() {
+        components: {
+            LinkTab
+        },
+        data() {
             return {
+                name: 'Laravel+Vue',
                 userDropdown: false,
             }
         },
